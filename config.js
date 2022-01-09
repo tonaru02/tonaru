9410adc3524798fb3c6d94cc421fde20ba73b355
tonaru02 <97376032+tonaru02@users.noreply.github.com>
2022-01-09 12:37:05 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "inlove",
+    introTitle: 'như',
+    introDesc: `Xin lỗi đã làm em thất vọng
+    tha lỗi cho anh nha:<`,
+    btnIntro: '^^hic^^',
+    title: 'Anh biết anh sai rồi',
+    desc: 'Cho anh cơ hội để sửa sai nha ',
+    btnYes: 'Anh xin lỗi',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Mình quay lại nha em ',
+    mess: 'Có gì nhắn qua mes nha',
+    messDesc: 'Xin lôi em nhiều :<.',
+    btnAccept: 'ấn vô để qua mess',
+    messLink: 'https://www.facebook.com/messages/t/100047879161039' 
+}
